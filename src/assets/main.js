e1be46479f671cdc7316a39be936876ebcef571b
andreyvasilenko <andrewvasilen@gmail.com>
2017-01-29 16:47:11 +0200

    Changed main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,39 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/andreyvasilenko.json',
+    dataType: 'jsonp',
+    success: function(data) {
+      userCodeschoolCourses(data.courses.completed)
+    }
+  });
 
+  function userCodeschoolCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
 });
